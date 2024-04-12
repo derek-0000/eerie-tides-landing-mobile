ec56e5a1f1ab8c396ea6b984993dd8100091960e
salvador-perez-1 <salvadorperezarras@gmail.com>
2024-04-12 00:56:17 -0600

    fix: add local images

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { useState } from "react";
 import { AppLoading } from "expo";
+import { firstHalfMembers, secondHalfMembers } from "./assets/constants/members";
 // import { ImageBackground } from "react-native";
 // import LinearGradient from "react-native-linear-gradient";
 import {
@@ -65,53 +66,25 @@ const App = () => {
 
         <View style={styles.teamContainer}>
           <View style={styles.teamColumn}>
+          {firstHalfMembers.map((member, index)=> (
             <TeamMember
-              name="Ivan Lua"
-              position="Game Developer"
-              image="https://avatars.githubusercontent.com/u/71847983?v=4"
-            />
-            <TeamMember
-              name="Derek Alvarado"
-              position="Music Producer"
-              image="https://media-dfw5-2.cdn.whatsapp.net/v/t61.24694-24/417065052_399587009533028_5320411093734619725_n.jpg?ccb=11-4&oh=01_ASAPk1TdywG-cMMH-syXDJbozJYnUA3vOLAV_Dbn0Lm0_A&oe=66231D23&_nc_sid=e6ed6c&_nc_cat=106"
-            />
-            <TeamMember
-              name="Salvador Perez"
-              position="Game Developer"
-              image="https://avatars.githubusercontent.com/u/134955611?v=4"
-            />
-            <TeamMember
-              name="Jonatan Caleb"
-              position="Game Developer"
-              image="https://avatars.githubusercontent.com/u/71664858?v=4"
-            />
+                key={index}
+                name={member.name}
+                position={member.position}
+                image={member.image}
+              />
+
+            ))}
           </View>
           <View style={styles.teamColumn}>
+            {secondHalfMembers.map((member, index)=> (
             <TeamMember
-              name="Carlos Salazar"
-              position="Game Developer"
-              image="https://avatars.githubusercontent.com/u/80538989?v=4"
-            />
-            <TeamMember
-              name="Carlos Acosta"
-              position="Game Developer"
-              image="https://avatars.githubusercontent.com/u/71846585?v=4"
-            />
-            <TeamMember
-              name="Mauricio Ruiz"
-              position="Graphic Designer"
-              image="https://media-dfw5-2.cdn.whatsapp.net/v/t61.24694-24/425016303_1363126774369231_137642594200464304_n.jpg?ccb=11-4&oh=01_ASBJi73_1c4DzfvJRig4WhPBW4WivTFSGpMAAZPPrh7dOQ&oe=661B0366&_nc_sid=e6ed6c&_nc_cat=106"
-            />
-            <TeamMember
-              name="Andres Villa"
-              position="Graphic Designer"
-              image="https://avatars.githubusercontent.com/u/134955611?v=4"
-            />
-            <TeamMember
-              name="Omar Ramos"
-              position="Story Designer"
-              image="https://media-dfw5-2.cdn.whatsapp.net/v/t61.24694-24/426180734_751318329965227_476812138117648195_n.jpg?ccb=11-4&oh=01_ASC7-rKkAkmhEISsd0eyQUqRv70iBiyswE-m2HfaSKY9Vg&oe=6620350A&_nc_sid=e6ed6c&_nc_cat=101"
-            />
+                key={index}
+                name={member.name}
+                position={member.position}
+                image={member.image}
+              />
+            ))}
           </View>
         </View>
       </View>
---
+++ assets/constants/members.ts
@@ -0,0 +1,52 @@
+export const firstHalfMembers = [
+    {
+        name: "Ivan Lua",
+        position: "Game Developer",
+        image: "/assets/members/lua.jpg"
+    },
+    {
+        name: "Derek Alvarado",
+        position: "Music Producer",
+        image: "/assets/members/derek.jpg"
+    },
+    {
+        name: "Salvador Perez",
+        position: "Game Developer",
+        image: "/assets/members/salvador.png"
+    },
+    {
+        name: "Jonatan Caleb",
+        position: "Game Developer",
+        image: "/assets/members/jonatan.png"
+    },
+    {
+        name: "Carlos Salazar",
+        position: "Game Developer",
+        image: "/assets/members/carlos.jpg"
+    }
+]
+
+
+export const secondHalfMembers = [
+    {
+      name: "Carlos Acosta",
+      position: "Game Developer",
+      image: "/assets/members/carloss.jpg"
+    },
+    {
+      name: "Mauricio Ruiz",
+      position: "Graphic Designer",
+      image: "/assets/members/mauricio.jpg"
+    },
+    {
+      name: "Andres Villa",
+      position: "Graphic Designer",
+      image: "/assets/members/villa.png"
+    },
+    {
+      name: "Omar Ramos",
+      position: "Story Designer",
+      image: "/assets/members/omar.jpg"
+    }
+  ];
+  
